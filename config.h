df05732c4dc60e4eece600a55c2f3d84e05c0c9f
Angelo Gambara <angelo@angelogambara.dev>
2026-01-13 20:05:33 +0100

    chore: edit BLOCKS

--- config.h
+++ config.h
@@ -2,10 +2,10 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER " "
 
 // Maximum number of Unicode characters that a block can output.
-#define MAX_BLOCK_OUTPUT_LENGTH 45
+#define MAX_BLOCK_OUTPUT_LENGTH 512
 
 // Control whether blocks are clickable.
 #define CLICKABLE_BLOCKS 1
@@ -18,15 +18,15 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("", "sb-mail", 600, 1)  \
-    X("", "sb-music", 0, 2)   \
-    X("", "sb-disk", 1800, 3) \
-    X("", "sb-memory", 10, 4) \
-    X("", "sb-loadavg", 5, 5) \
-    X("", "sb-mic", 0, 6)     \
-    X("", "sb-record", 0, 7)  \
-    X("", "sb-volume", 0, 8)  \
-    X("", "sb-battery", 5, 9) \
-    X("", "sb-date", 1, 10)
+    /* X("", "sb-disk",        900, 10) */ \
+    X("", "sb-mail",        900,  9) \
+    X("", "sb-memory",       10,  8) \
+    X("", "sb-cpu",          10,  7) \
+    X("", "sb-bluetooth",    10,  6) \
+    X("", "sb-network",       5,  3) \
+    X("", "sb-brightness",    0,  5) \
+    X("", "sb-volume",        0,  4) \
+    X("", "sb-battery",       5,  2) \
+    X("", "sb-date",          1,  1)
 
 #endif  // CONFIG_H
